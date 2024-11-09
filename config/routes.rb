7f47a1270701b1f1a2aeb69f0d3ac22007df1990
Rodrigo Medina <rodrigo-medina@uiowa.edu>
2024-11-09 04:54:20 +0000

    Ran rails commands to generate controller and model for logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/new"
+  get "users/create"
   get "characters/index"
   get "characters/show"
   get "characters/new"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_09_024802) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_09_044145) do
   create_table "characters", force: :cascade do |t|
     t.integer "health"
     t.integer "experience"
@@ -30,6 +30,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_09_024802) do
   create_table "user_profiles", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
