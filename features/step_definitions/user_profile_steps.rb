2cce8c3b82cab2eba7e9e69e7310a78103e4ee58
yyjohn <yiyang-shen@uiowa.edu>
2024-11-12 16:07:05 -0600

    Balance half working, start working on controller

--- features/step_definitions/user_profile_steps.rb
+++ features/step_definitions/user_profile_steps.rb
@@ -1,14 +1,15 @@
 Given(/^I have an account$/) do
   @user = User.create!(username: 'awesomehawkeye', email: 'awesome@uiowa.edu', password_digest: '12345')
-  # @grid = Grid.create!(grid_id: 1, name: 'world')
-  # @cell = Cell.create!(cell_id: 1, cell_loc: '1A', mons_prob: 0.3, disaster_prob: 0.3, weather: 'Sunny',
-  #                      terrain:'desert', has_store: true, grid_id: @grid.grid_id)
-  # @item = Item.create!()
-  # @inventory = Inventori.create!()
-  # TODO: create other models with minimum code and create dummy variables
+  @grid = Grid.create!(grid_id: 1, name: 'earth')
+  @cell = Cell.create!(cell_id: 1, cell_loc: '1A', mons_prob: 0.3, disaster_prob: 0.3, weather: 'Sunny',
+                       terrain:'desert', has_store: true, grid_id: @grid.grid_id)
+  @item1 = Item.create!(item_id: 1, name: 'thingy', category: 'sword', cost: 1)
+  @item2 = Item.create!(item_id: 2, name: 'stuff', category: 'shield', cost: 1)
+
+  @inventory = Inventory.create!(inv_id: 1, items: [@item1, @item2])
   @character = Character.create!(username: @user.username, character_name: 'Hawkeye',
                                     shard_balance: 0, health: 100, experience: 0, level: 1,
-                                    grid_id: 1, cell_id: 1, inv_id: 1)
+                                    grid_id: @grid.grid_id, cell_id: @cell.cell_id, inv_id: @inventory.inv_id)
   # visit new_user_session_path
   # fill_in 'Email', with: @user.email
   # fill_in 'Password', with: 'password'
