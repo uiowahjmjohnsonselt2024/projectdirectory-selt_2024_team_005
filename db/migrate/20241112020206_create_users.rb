dad1603ea4a75b1019cab26283ae7e0183c08c42
yyjohn <yiyang-shen@uiowa.edu>
2024-11-12 11:43:37 -0600

    FIXED DB sequence and migration issues

--- db/migrate/20241112020206_create_users.rb
+++ db/migrate/20241112020206_create_users.rb
@@ -6,22 +6,6 @@ class CreateUsers < ActiveRecord::Migration[7.2]
       t.string :email, null: false
       t.timestamps
     end
-    create_table :characters do |t|
-      t.belongs_to :user, null: false, foreign_key: true
-      t.string :character_name, null: false, primary_key: true
-      t.string :username, null: false
-      t.integer :health, null: false
-      t.integer :shard_balance, null: false
-      t.integer :experience, null: false
-      t.integer :level, null: false
-      t.integer :grid_id, null: false
-      t.integer :cell_id, null: false
-      t.integer :inv_id, null: false
-      t.timestamps
-    end
-    add_foreign_key :characters, :grids, column: :grid_id
-    add_foreign_key :characters, :cells, column: :cell_id
-    add_foreign_key :characters, :inventories, column: :inv_id
     add_index :users, :username, unique: true
     add_index :users, :email, unique: true
   end
---
+++ db/migrate/20241112025800_create_characters.rb
@@ -0,0 +1,20 @@
+class CreateCharacters < ActiveRecord::Migration[7.2]
+  def change
+    create_table :characters, id: false do |t|
+          t.string :character_name, null: false, primary_key: true
+          t.string :username, null: false
+          t.integer :health, null: false
+          t.integer :shard_balance, null: false
+          t.integer :experience, null: false
+          t.integer :level, null: false
+          t.integer :grid_id, null: false
+          t.integer :cell_id, null: false
+          t.integer :inv_id, null: false
+          t.timestamps
+        end
+    add_foreign_key :characters, :users, column: :username, primary_key: :username
+    add_foreign_key :characters, :grids, column: :grid_id, primary_key: :grid_id
+    add_foreign_key :characters, :cells, column: :cell_id, primary_key: :cell_id
+    add_foreign_key :characters, :inventories, column: :inv_id, primary_key: :inv_id
+  end
+end
