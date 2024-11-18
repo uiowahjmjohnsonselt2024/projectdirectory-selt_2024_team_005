ad3cd35486614d954516cbf2f3ca2878733e192a
rmdna <rodrigo-medina@uiowa.edu>
2024-11-18 16:24:01 -0600

    Working back loginpage code

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     end
   end
 
+  get "user/new"
+  get "users/create"
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
