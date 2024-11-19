15c3a79ca7684c965b83c87471e20368becc5272
Rodrigo Medina <rodrigo-medina@uiowa.edu>
2024-11-18 18:55:25 -0600

    Added password validation to user.rb in app models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_one :character, foreign_key: "username", primary_key: "username",  dependent: :destroy
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 4 }
 end
