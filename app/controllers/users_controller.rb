911bc27c0540aad7b40cd3943aeb62171d124875
yyjohn <yiyang-shen@uiowa.edu>
2024-11-18 22:06:52 -0600

    fixed rates for buying shards – about to implement API call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,26 @@ class UsersController < ApplicationController
   end
 
   def buy_shards
+    @currencies = %w[USD EUR GBP JPY AUD CAD CHF CNY SEK NZD]
     @user = User.find_by!(username: params[:username])
     @character = Character.find_by(username: params[:username])
   end
 
   def process_payment
     @user = User.find_by!(username: params[:username])
-    @shards = params[:shards].to_i
+    # @shards = params[:shards].to_i
 
-    # Validate the selected shards amount
-    unless [ 5, 10, 15, 20 ].include?(@shards)
-      flash[:alert] = "Invalid shards amount selected."
+    @amount = params[:amount].to_f
+    @currency = params[:currency]
+    # Validate inputs
+    if @amount <= 0
+      flash[:alert] = "Please enter a valid amount."
       redirect_to buy_shards_user_path(@user.username) and return
     end
+    # Currency exchange
+    exchange_rate = get_exchange_rate(@currency)
+    amount_in_usd = @amount / exchange_rate
+    shards_to_credit = amount_in_usd.floor
 
     # Validate credit card information (basic validation)
     cc_number = params[:cc_number]
@@ -55,15 +62,32 @@ class UsersController < ApplicationController
       redirect_to user_path(@user.username) and return
     end
 
-    @character.shard_balance += @shards
+    @character.shard_balance += shards_to_credit
 
     if @character.save
-      flash[:notice] = "Successfully purchased #{@shards} shards."
+      flash[:notice] = "Successfully purchased #{shards_to_credit} shards."
       redirect_to user_path(@user.username)
     else
       flash[:alert] = "Unable to update shard balance."
       redirect_to user_path(@user.username)
     end
+  end
+
+  def get_exchange_rate(currency)
+    # Simulate exchange rates for the purpose of this mock system
+    exchange_rates = {
+      'USD' => 1.0,
+      'EUR' => 0.85,
+      'GBP' => 0.75,
+      'JPY' => 110.0,
+      'AUD' => 1.35,
+      'CAD' => 1.25,
+      'CHF' => 0.90,
+      'CNY' => 6.5,
+      'SEK' => 8.5,
+      'NZD' => 1.4
+    }
+    exchange_rates[currency] || 1.0
   end
 
   def destroy
