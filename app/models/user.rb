54f5a175051e98de0c5f1aeab633c210410e4fa0
Anna Davis <anna-davis@uiowa.edu>
2024-11-21 16:09:56 -0600

    getting routes working with login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 require "open_exchange_rates"
 class User < ApplicationRecord
+  has_secure_password
   has_one :character, foreign_key: "username", primary_key: "username",  dependent: :destroy
   OpenExchangeRates.configure do |config|
     config.app_id = "541c6dbbdf244c82bd71151575e47f27"
