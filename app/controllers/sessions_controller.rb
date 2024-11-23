69a9e82eaed38eea23ee166abcdb659b72b14695
Anna Davis <anna-davis@uiowa.edu>
2024-11-23 13:28:57 -0600

    fixing routes to validate user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,22 @@
 class SessionsController < ApplicationController
-  # Show the login form
-  def new
-  end
-
-  # Handle the login process
   def create
     # Find the user by username
     @user = User.find_by(username: params[:username])
 
     # Check if user exists and password is correct
     if @user && @user.authenticate(params[:password])
-      # Set up session
-      session[:user_id] = @user.id  # Save user ID in session to keep user logged in
-      flash.now[:notice] = "Successfully logged in!"
-      redirect_to home_path  # Redirect to home page
-    elsif !(@user && @user.authenticate(params[:password]))
-      flash.now[:alert] = "Invalid username or password"
-      render :new  # Render the login form again
+      # Set up session with username instead of id
+      session[:username] = @user.username  # Save username in session
+      flash[:notice] = "Successfully logged in!"
+      redirect_to home_path
     else
-      flash.now[:notice] = ""
+      flash.now[:alert] = "Invalid username or password"
+      render :new
     end
   end
 
-  # Handle the logout process
   def destroy
-    session[:user_id] = nil
+    session[:username] = nil  # Clear the username from session
     redirect_to login_path, notice: "You have been logged out."
   end
 end
