d0ad0b530efb266406f1f85d62effed8d123890d
liam-wells <liamwells538@gmail.com>
2024-11-27 00:23:47 -0600

    Set default empty inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,10 @@
 class Inventory < ApplicationRecord
+  before_save :set_default_items
+
+  private
+
+  def set_default_items
+    self.items = (items || []).first(12) # Inventory only takes first 12 items defined
+    self.items += Array.new(12 - self.items.size, nil) # Pad with nil up to 12
+  end
 end
