484cc296004bc52c9cb0a49ede78166779ddb0d2
Rodrigo Medina <rodrigo-medina@uiowa.edu>
2024-11-30 18:49:54 -0600

    app\channels\multiplayer_channel.rb

--- app/channels/multiplayer_channel.rb
+++ app/channels/multiplayer_channel.rb
@@ -1,24 +1,5 @@
 class MultiplayerChannel < ApplicationCable::Channel
   def subscribed
-<<<<<<< HEAD
-    # Called when a user subscribes to the channel (enters the world)
-    stream_from "multiplayer_#{params[:world_id]}" # Broadcasting to a specific world
-
-    # Notify other players that this user has joined
-    ActionCable.server.broadcast(
-      "multiplayer_#{params[:world_id]}",
-      message: "#{current_user.username} has entered the world."
-    )
-  end
-
-  def unsubscribed
-    # Called when a user unsubscribes (leaves the world)
-    ActionCable.server.broadcast(
-      "multiplayer_#{params[:world_id]}",
-      message: "#{current_user.username} has left the world."
-    )
-  end
-=======
     stream_from "multiplayer_channel_#{params[:room]}"
   end
 
@@ -35,5 +16,4 @@ class MultiplayerChannel < ApplicationCable::Channel
   # def handle_move(direction)
   #   # Move logic
   # end
->>>>>>> bd3e9e9115042199b9e47c0ac8507840173daca7
 end
