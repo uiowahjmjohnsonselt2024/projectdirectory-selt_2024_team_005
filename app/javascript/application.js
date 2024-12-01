d2362a0168cb1c4e86ca6ab6cba544ebd533af9e
Rodrigo Medina <rodrigo-medina@uiowa.edu>
2024-11-30 18:48:18 -0600

    resolved app\javascript\application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,15 +10,4 @@ const application = Application.start()
 
 import "@hotwired/turbo-rails"
 import "controllers"
-<<<<<<< HEAD
-import "channels/multiplayer_channel"
-import "channels/chat_channel"
-import "channels/application_channel"
-import "channels/consumer"
-import "stimulus"
-import Rails from "@rails/ujs";
-Rails.start();
-
-=======
 import "channels"
->>>>>>> bd3e9e9115042199b9e47c0ac8507840173daca7
