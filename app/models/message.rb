033ca217112be08113afd70c5e6784015d4a276a
mirrorigin <jingmliang@uiowa.edu>
2024-12-06 17:07:25 -0600

    Set message storage limitations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,17 @@
 class Message < ApplicationRecord
+
+  after_create :limit_user_messages
+
+  private
+
+  def limit_user_messages
+    # Find user's message and sort it by creation time
+    user_messages = Message.where(username: username).order(created_at: :desc)
+
+    # If there are more than 100 messages, delete the redundant ones
+    if user_messages.count > 100
+      messages_to_delete = user_messages.offset(100)
+      Message.where(id: messages_to_delete.pluck(:id)).delete_all
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_06_223217) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_230427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_06_223217) do
     t.string "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username", "created_at"], name: "index_messages_on_username_and_created_at"
   end
 
   create_table "potions", primary_key: "potion_id", id: :serial, force: :cascade do |t|
