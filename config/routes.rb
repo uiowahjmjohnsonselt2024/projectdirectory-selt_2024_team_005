ad6e6a195ed565fdca3bd9ff42faa4634e44f7b8
yyjohn <yiyang-shen@uiowa.edu>
2024-12-09 18:44:18 -0600

    routing and controller for monster ascii

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
     member do
       post "bribe_monster"
       post "fight_monster"
+      get "monster_ascii", to: "characters#get_monster_ascii"
     end
   end
 
