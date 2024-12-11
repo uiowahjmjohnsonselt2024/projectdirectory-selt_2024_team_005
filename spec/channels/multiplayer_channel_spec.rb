b8e41f8715d616740a2348ee82c0633a81c473d5
Rodrigo Medina <rodrigo-medina@uiowa.edu>
2024-12-10 18:53:43 -0600

    trying to write tests for multiplayer_channel.rb

--- spec/channels/multiplayer_channel_spec.rb
+++ spec/channels/multiplayer_channel_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe MultiplayerChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(username: 'herky', email: 'herky@uiowa.edu', password_digest: '12345') }
+  let(:grid) { Grid.create!(id: 1, name: 'Europa') }
+  let(:character) { Character.create!(username: user.username, character_name: 'Herky', level: 1, grid_id: grid.id, cell_id: 1, online_status: false) }
+
+  before do
+    stub_connection(current_user: user)
+    character # Ensure the character is created
+  end
+
+  describe 'subscribing to the channel' do
+    it 'confirms subscription and streams from the correct grid' do
+      subscribe
+      expect(subscription).to be_confirmed
+      expect(subscription).to have_stream_from("multiplayer_grid_#{character.grid_id}")
+    end
+  end
+
 end
